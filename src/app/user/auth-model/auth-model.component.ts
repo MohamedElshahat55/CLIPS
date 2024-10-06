2d7749f029fce35271e1a0582c2c27c6eb06b4de
MohamedElshahat55 <mohamedelshahat582@gmail.com>
2024-10-06 23:43:32 +0300

    create a shared module and modal component in shared module and import the shared module in user module

---
+++ src/app/user/auth-model/auth-model.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-model',
+  templateUrl: './auth-model.component.html',
+  styleUrl: './auth-model.component.css'
+})
+export class AuthModelComponent {
+
+}
