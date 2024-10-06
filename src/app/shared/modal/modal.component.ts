6a8ea5e5e8370fdf69aaed6ea88455d2c14f1651
MohamedElshahat55 <mohamedelshahat582@gmail.com>
2024-10-07 00:01:38 +0300

    create a modalService to handel toggling the auth modal

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ModalService } from '../../services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrl: './modal.component.css'
+  styleUrl: './modal.component.css',
 })
 export class ModalComponent {
-
+  constructor(private _modalService: ModalService) {
+    console.log(this._modalService.visiable);
+  }
 }
