5b321228bb7a35cea52441c6e089c5a4c9663c8c
MohamedElshahat55 <mohamedelshahat582@gmail.com>
2024-10-08 17:07:44 +0300

    handel login form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('Testing193!', [
+      Validators.required,
+      Validators.pattern(
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
+      ), //Testing193!
+    ]),
+  });
 
+  get email() {
+    return this.loginForm.controls.email;
+  }
+  get password() {
+    return this.loginForm.controls.password;
+  }
+
+  login() {
+    console.log(this.loginForm.value);
+  }
 }
