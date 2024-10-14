ba9241f4083a33047fa271cb93d874b3f42d1d01
MohamedElshahat55 <mohamedelshahat582@gmail.com>
2024-10-14 18:03:16 +0300

    Querying the clips from db

--- src/app/services/clips.service.ts
+++ src/app/services/clips.service.ts
@@ -3,9 +3,12 @@ import {
   AngularFirestore,
   DocumentReference,
   AngularFirestoreCollection,
+  QuerySnapshot,
 } from '@angular/fire/compat/firestore';
 
 import IClip from '../models/clip.model';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { map, of, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,11 +16,24 @@ import IClip from '../models/clip.model';
 export class ClipsService {
   public clipsCollection: AngularFirestoreCollection<IClip>;
 
-  constructor(private _db: AngularFirestore) {
+  constructor(private _db: AngularFirestore, private _auth: AngularFireAuth) {
     this.clipsCollection = _db.collection('clips');
   }
 
   createClip(clipData: IClip): Promise<DocumentReference<IClip>> {
     return this.clipsCollection.add(clipData);
+  }
+
+  getUserClips() {
+    return this._auth.user.pipe(
+      switchMap((user) => {
+        if (!user) {
+          return of([]);
+        }
+        const query = this.clipsCollection.ref.where('uid', '==', user.uid);
+        return query.get();
+      }),
+      map((snapshot) => (snapshot as QuerySnapshot<IClip>).docs)
+    );
   }
 }
--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ClipsService } from '../../services/clips.service';
+import IClip from '../../models/clip.model';
 
 @Component({
   selector: 'app-manage',
@@ -8,11 +10,25 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ManageComponent implements OnInit {
   videoOrder = '1';
-  constructor(private _router: Router, private _route: ActivatedRoute) {}
+  clips: IClip[] = [];
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _clipService: ClipsService
+  ) {}
 
   ngOnInit(): void {
     this._route.queryParams.subscribe((params) => {
       this.videoOrder = params['sort'];
+    });
+    this._clipService.getUserClips().subscribe((docs) => {
+      this.clips = [];
+      docs.forEach((doc) => {
+        this.clips.push({
+          docID: doc.id,
+          ...doc.data(),
+        });
+      });
     });
   }
   sort(e: Event) {
